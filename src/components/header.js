982f96b5045514eaa85e3c7dae89132f617799f0
Tim <ryalin1286@gmail.com>
2021-03-09 10:40:52 +0000

    finished nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,7 @@ const Header = () => {
         ))}
       </NavMenu>
       <NavBtn>
-        <Button>Book a Flight</Button>
+        <Button primary="true" round="true" to="/trips">Book a Flight</Button>
       </NavBtn>
     </Nav>
   )
